315d593199fd1539a1bdef6a44bda75e0a61df0d
Finnegan R. Manthe <finnrmanthe@gmail.com>
2026-04-20 22:00:02 -0700

    Revert "Updated test_main"
    This reverts commit 7a0dcec367073725003b53c0845e17d93b317c21.

--- test_main.py
+++ test_main.py
@@ -1,5 +1,4 @@
-from inventory import Inventory
-from product import Product
+from assignment2.main import Product, Inventory
 
 class TestInventory:
     def test_product_class(self):
@@ -9,29 +8,22 @@ class TestInventory:
         assert product.price == 100
 
     def test_inventory_constructor(self):
-        print("Running tests on inventory constructor")
         names = ["Apple", "Cucumber"]
         stocks = [55, 66]
         prices = [1.99, 2.99]
         inventory = Inventory(names, stocks, prices)
 
-        assert inventory.get_total_products() == 2
-        assert inventory.get_product(0).name == "Apple"
-        assert inventory.get_product(0).stock == 55
-        assert inventory.get_product(0).price == 1.99
+        products = inventory.get_products()
+        assert len(products) == 2
+        assert products[0].name == "Apple"
+        assert products[0].stock == 55
+        assert products[0].price == 1.99
 
-        assert inventory.get_product(1).name == "Cucumber"
-        assert inventory.get_product(1).stock == 66
-        assert inventory.get_product(1).price == 2.99
-
-        for i in range(inventory.get_total_products()):
-            self.assert_product(inventory.get_product(i),names[i],stocks[i],prices[i])
+        assert products[1].name == "Cucumber"
+        assert products[1].stock == 66
+        assert products[1].price == 2.99
 
     def assert_product(self, product, name, stock, price):
-        print('Testing product attribute strings conversions')
         assert name in product.__str__()
-        assert str(stock) in product.__str__()
-        assert str(price) in product.__str__()
-
-test = TestInventory()
-test.test_inventory_constructor()
+        assert stock in product.__str__()
+        assert price in product.__str__()
